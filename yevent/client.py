3aaa94c74715c8902429ba614660ca01c926a0a6
mamo.yang <mamo.yang@ucloud.cn>
2019-06-26 01:04:51 +0800

    add yevent base

---
+++ yevent/client.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+from socket import *
+
+def main():
+  HOST ='localhost'
+  PORT = 9981
+  BUFFSIZE = 2048
+  ADDR = (HOST, PORT)
+
+  client = socket(AF_INET, SOCK_STREAM)
+  client.connect(ADDR)
+  data = "hello, world" 
+  print("input data:" + data)
+  client.send(data.encode())
+  data = client.recv(BUFFSIZE).decode()
+  print(data)
+  client.close()
+
+if __name__ == '__main__':
+  main()
